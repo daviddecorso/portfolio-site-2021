c0d15ff8f647acaf3013f2555030e6f20599c4e2
David DeCorso <davidadecorso@gmail.com>
2021-05-30 03:19:57 -0400

    about page intro and experience sections completed

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 const content = document.getElementById("body");
 
+const tab1 = document.getElementById("tab1");
+const tab2 = document.getElementById("tab2");
+
+const exp1 = document.getElementById("exp1");
+const exp2 = document.getElementById("exp2");
+
+let activeTab = exp1;
+
 const ss = new SmoothScroll({
   container: content,
 
@@ -9,3 +17,26 @@ const ss = new SmoothScroll({
 
   intertia: 1,
 });
+
+toggleTabs = () => {
+  tab1.classList.toggle("tab-is-active");
+  tab1.classList.toggle("tab-is-inactive");
+  tab2.classList.toggle("tab-is-active");
+  tab2.classList.toggle("tab-is-inactive");
+};
+
+toggleExp = (makeActive) => {
+  activeTab.classList.toggle("is-hidden");
+  makeActive.classList.toggle("is-hidden");
+  activeTab = makeActive;
+};
+
+tab1.onclick = () => {
+  toggleTabs();
+  toggleExp(exp1);
+};
+
+tab2.onclick = () => {
+  toggleTabs();
+  toggleExp(exp2);
+};
