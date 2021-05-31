00b6d9cc5fc5ac54494908eb294dc28a37a59816
David DeCorso <davidadecorso@gmail.com>
2021-05-31 16:44:52 -0400

    Other projects section done

--- src/main.js
+++ src/main.js
@@ -1,14 +1,6 @@
 const content = document.getElementById("body");
 
-const tab1 = document.getElementById("tab1");
-const tab2 = document.getElementById("tab2");
-
-const exp1 = document.getElementById("exp1");
-const exp2 = document.getElementById("exp2");
-
-let activeTab = exp1;
-
-const ss = new SmoothScroll({
+/* const ss = new SmoothScroll({
   container: content,
 
   threshold: 0.5,
@@ -17,26 +9,16 @@ const ss = new SmoothScroll({
 
   intertia: 1,
 });
+ */
 
-toggleTabs = () => {
-  tab1.classList.toggle("tab-is-active");
-  tab1.classList.toggle("tab-is-inactive");
-  tab2.classList.toggle("tab-is-active");
-  tab2.classList.toggle("tab-is-inactive");
-};
+import ASScroll from "https://cdn.skypack.dev/@ashthornton/asscroll";
 
-toggleExp = (makeActive) => {
-  activeTab.classList.toggle("is-hidden");
-  makeActive.classList.toggle("is-hidden");
-  activeTab = makeActive;
-};
+const asscroll = new ASScroll({
+  customScrollbar: true,
+  scrollbarHandleEl: ".my-scrollbar-handle",
+  scrollbarStyles: true,
+});
 
-tab1.onclick = () => {
-  toggleTabs();
-  toggleExp(exp1);
-};
-
-tab2.onclick = () => {
-  toggleTabs();
-  toggleExp(exp2);
-};
+window.addEventListener("load", () => {
+  asscroll.enable();
+});
