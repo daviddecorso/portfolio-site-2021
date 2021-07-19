1c4ae8371511fc3148ddb734380f9e2c26ba3a03
David DeCorso <davidadecorso@gmail.com>
2021-07-19 01:40:08 -0400

    Added QR docent project to homepage and dev log page, optimized images

--- src/main.js
+++ src/main.js
@@ -9,3 +9,8 @@ const asscroll = new ASScroll({
 window.addEventListener("load", () => {
   asscroll.enable();
 });
+
+// Message to devs
+console.log("%c How do you do fellow dev? 👋", "color: #00F8F8; font-weight: bold; font-size: 18px;");
+console.log("%c Below is a link to this site's GitHub.", "color: #00F8F8; font-size: 15px;");
+console.log("%c https://github.com/daviddecorso/portfolio-site-2021", "color: #EC004D;");
